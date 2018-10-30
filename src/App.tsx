2345db0a3d0d00ba3f8d8d078a70451d14bdd9bb
chenzj <chjczj@126.com>
2018-10-30 17:31:14 +0800

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { withStyles } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
+import InputBase from '@material-ui/core/InputBase';
 import * as React from 'react';
 import SwipeableViews from 'react-swipeable-views';
 import { mod } from 'react-swipeable-views-core';
@@ -7,45 +8,53 @@ import { virtualize } from 'react-swipeable-views-utils';
 const VirtualizeSwipeableViews = virtualize(SwipeableViews);
 const styles = () => ({
   root: {
-    display: "flex",
-    justify: "center"
+    display: "flex"
   }
 });
 class App extends React.Component<any, any> {
+  public data = require('./data.json');
+  public state = { n: 0 }
+  public m = (i: number) => (mod(i, this.data.length))
   public render() {
-    const slideRenderer = (params: any) => {
-      const data = require('./data.json');
-      const { index, key } = params;
-      const slideContent = (n: any) => {
-        return (
-          <a href={data[n].href} key={key}>
-            <img width="100%" src={data[n].src} />
-          </a>
-        )
-      }
-      switch (mod(index, data.length)) {
-        case 0:
-          return(
-            slideContent(0)
-          )
-        case 1:
-          return(
-            slideContent(1)
-          )
-        default:
-          return (null)
-      }
-    }
-    const { classes } = this.props;
+    const t = this;
+    const { classes } = t.props;
     return (
-      <Grid className={classes.root}>
-        <Grid xs={8} xl={2}>
-          <Grid className={classes.demo} justify="center">
-            <VirtualizeSwipeableViews slideRenderer={slideRenderer} />
-          </Grid>
+      <Grid container={true} className={classes.root} justify="center">
+        <Grid item={true} xs={8} xl={2}>
+          <VirtualizeSwipeableViews
+            resistance={true}
+            slideRenderer={t.sR}
+            onChangeIndex={t.hS}
+          />
+        </Grid>
+        <Grid item={true} xs={10} xl={4}>
+          <InputBase />
         </Grid>
       </Grid>
     )
+  }
+  private sR = (params: any) => {
+    const t = this;
+    const { index, key } = params;
+    const sC = (n: number) => {
+      const td = t.data[n]
+      return (
+        <a href={td.href} key={key}>
+          <img width="100%" src={td.src} />
+        </a>
+      )
+    }
+    switch (t.m(index)) {
+      case 0:
+        return (sC(0))
+      case 1:
+        return (sC(1))
+      default:
+        return (null)
+    }
+  }
+  private hS = (n: number) => {
+    this.setState({ n })
   }
 }
 export default withStyles(styles)(App);
